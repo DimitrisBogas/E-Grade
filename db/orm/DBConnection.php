1374e51511ed2020343368910b3f084b20a7b994
DimitrisBogas <bj2006s@hotmail.com>
2015-12-12 14:36:11 +0200

    code refactor

--- db/orm/DBConnection.php
+++ db/orm/DBConnection.php
@@ -16,11 +16,8 @@ class DBConnection {
         $this->dbHandle = mysql_connect($this->dbSettings->host,$this->dbSettings->username, $this->dbSettings->password);
     }
     public function query ($query) {
-        mysql_select_db($this->dbSettings->dBName);
+        mysql_select_db($this->dbSettings->dbName);
         $result = mysql_query($query);
         return $result;
-    }
-    public function getDataaseName() {
-        return $this->dbName;
     }
 }
--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-           // include 'db/connection/DBSettings.php';
-            include 'db/orm/DBConnection.php';
-            include 'db/orm/QueryBuilder/UserQueryBuilder.php';
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+// include 'db/connection/DBSettings.php';
+include 'db/orm/DBConnection.php';
+include 'db/orm/QueryBuilder/UserQueryBuilder.php';
 
-            echo "E-Grade Project </br>";
-            $dBConnection = new DBConnection();
-            $userQueryBuilder = new UserQueryBuilder();
-            $result  = $dBConnection->query($userQueryBuilder->showUsers());
-      //      $result = $dBConnection->query($userQueryBuilder->createSecretariat(1, "secu", "secp", "SECRETARIAT"));
-        echo "$result </br>";
-            mysql_data_seek($result, 0);
-            $row = mysql_fetch_assoc($result);
-            $formdata="username";
-            echo $row[$formdata];
-        $result = $dBConnection->query($userQueryBuilder->createSecretariat('secu', 'secp', 'SECRETARY'));
-        echo "$result </br>";
+echo "E-Grade Project </br>";
+$dBConnection = new DBConnection();
+$userQueryBuilder = new UserQueryBuilder();
+$result = $dBConnection->query($userQueryBuilder->showUsers());
+//      $result = $dBConnection->query($userQueryBuilder->createSecretariat(1, "secu", "secp", "SECRETARIAT"));
+echo "$result </br>";
+mysql_data_seek($result, 0);
+$row = mysql_fetch_assoc($result);
+$formData = "username";
+echo $row[$formData];
+$result = $dBConnection->query($userQueryBuilder->createSecretariat('secu', 'secp'));
+echo "$result </br>";
 //  echo $userQueryBuilder->createSecretariat();
-          //  echo $result;
-           // print_r($obj);
-        ?>
-    </body>
+//  echo $result;
+// print_r($obj);
+?>
+</body>
 </html>
