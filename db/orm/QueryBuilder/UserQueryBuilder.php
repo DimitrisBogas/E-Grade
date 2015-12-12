b8c932cb5935af18a8930e736b3a54b84bb5baa7
DimitrisBogas <bj2006s@hotmail.com>
2015-12-12 13:46:12 +0200

    user types enum & insert more create roles in user query builder

--- db/orm/QueryBuilder/UserQueryBuilder.php
+++ db/orm/QueryBuilder/UserQueryBuilder.php
@@ -1,15 +1,36 @@
 <?php
+include 'db/orm/QueryBuilder/UserTypes.php';
+
 class UserQueryBuilder {
+    private  $userType;
     private $dBName;
     public function __construct() {
         $db = new DBSettings();
         $dbSettings = $db->getDbSettings();
         $this->dBName= $dbSettings->dBName;
+        $this->userType = new UserTypes(); 
     }
-    public function createSecretariat($user, $password, $usertype) {
-     return ("INSERT INTO `$this->dBName`.`users` (`username`, `password`, `usertype`) VALUES ('$user', '$password', '$usertype')");
- }
-    public function showusers() {
+    public function createSecretariat($user, $password) {
+     $userType = $this->userType->secretariat();
+     return ("INSERT INTO `$this->dBName`.`users` (`username`, `password`, `usertype`) VALUES ('$user', '$password', '$userType')");
+    }
+    
+    public function createStudent ($user, $password) {
+     $userType = $this->userType->student();
+     return ("INSERT INTO `$this->dBName`.`users` (`username`, `password`, `usertype`) VALUES ('$user', '$password', '$userType')");
+    }
+    
+     public function createProfessor ($user, $password) {
+     $userType = $this->userType->professor();
+     return ("INSERT INTO `$this->dBName`.`users` (`username`, `password`, `usertype`) VALUES ('$user', '$password', '$userType')");
+    }
+    
+    public function createAdministrator ($user, $password) {
+     $userType = $this->userType->administrator();
+     return ("INSERT INTO `$this->dBName`.`users` (`username`, `password`, `usertype`) VALUES ('$user', '$password', '$userType')");
+    }
+ 
+    public function showUsers() {
         return("select * from users");
     }
-}
+    }
---
+++ db/orm/QueryBuilder/UserTypes.php
@@ -0,0 +1,18 @@
+<?php
+class UserTypes {
+    function administrator () {
+        return "ADMINISTRATOR";
+    }
+    
+    function secretariat () {
+        return "SECRETARY";
+    }
+    
+    function professor () {
+        return "PROFESSOR";
+    }
+    
+    function student () {
+        return "STUDENT";
+    }
+}
