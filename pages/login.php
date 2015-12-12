48e83c2e1782fae73ada5589dc9870a9a9ea6749
DimitrisBogas <bj2006s@hotmail.com>
2015-12-12 19:37:41 +0200

    created php pages

---
+++ pages/login.php
@@ -0,0 +1,6 @@
+<form action="pages/loginVerification.php" method="post">
+    Username:<input type="text" name="username"><br>
+    Password:<input type="password" name="password"><br>
+    <br>
+    <input type="submit" value="Sign In">
+</form>
