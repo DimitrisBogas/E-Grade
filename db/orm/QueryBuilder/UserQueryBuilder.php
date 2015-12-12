15df09cf5c850257ea6b17c652064b2c74167a35
DimitrisBogas <bj2006s@hotmail.com>
2015-12-12 17:02:24 +0200

    minor changes

--- db/orm/QueryBuilder/UserQueryBuilder.php
+++ db/orm/QueryBuilder/UserQueryBuilder.php
@@ -12,25 +12,32 @@ class UserQueryBuilder {
     }
     public function createSecretariat($user, $password) {
      $userType = $this->userType->secretariat();
-        return ("INSERT INTO $this->dbName.users (username, password, usertype) VALUES ('$user', '$password', '$userType')");
+        return ("INSERT INTO $this->dbName.users (user.username, user.password, user.usertype) VALUES ('$user', '$password', '$userType')");
     }
 
     public function createStudent ($user, $password) {
      $userType = $this->userType->student();
-        return ("INSERT INTO $this->dbName.users (username, password, usertype) VALUES ('$user', '$password', '$userType')");
+        return ("INSERT INTO $this->dbName.users (user.username, user.password, user.usertype) VALUES ('$user', '$password', '$userType')");
     }
 
      public function createProfessor ($user, $password) {
      $userType = $this->userType->professor();
-         return ("INSERT INTO $this->dbName.users (username, password, usertype) VALUES ('$user', '$password', '$userType')");
+         return ("INSERT INTO $this->dbName.users (user.username, user.password, user.usertype) VALUES ('$user', '$password', '$userType')");
     }
 
     public function createAdministrator ($user, $password) {
      $userType = $this->userType->administrator();
-        return ("INSERT INTO $this->dbName.users (username, password, usertype) VALUES ('$user', '$password', '$userType')");
+        return ("INSERT INTO $this->dbName.users (user.username, user.password, user.usertype) VALUES ('$user', '$password', '$userType')");
     }
  
     public function showUsers() {
         return("select * from users");
     }
+
+    public function login($username, $password)
+    {
+        $query = "select username, password from users where username = $username and password = $password";
+        //return null;
+    }
+
     }
